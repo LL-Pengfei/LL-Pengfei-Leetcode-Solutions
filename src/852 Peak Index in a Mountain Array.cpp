863e65d2acfda79bdd9f3df64e51992ec330ca09
LL-Pengfei <42534460+LL-Pengfei@users.noreply.github.com>
2019-10-28 15:19:34 +0800

    link fix /20191028

--- src/852 Peak Index in a Mountain Array.cpp
+++ src/852 Peak Index in a Mountain Array.cpp
@@ -1,5 +1,5 @@
 //852. Peak Index in a Mountain Array
-//https://leetcode.com/problems/peak-index-in-a-mountain-array/submissions/
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
 
 class Solution {
 public:
